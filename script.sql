8d22ecc60cfd1cb98bffb4e7e9c4e9b60d88e709
nderhore <58103193+nderhore@users.noreply.github.com>
2023-10-09 19:39:32 +0200

    Update script.sql

--- script.sql
+++ script.sql
@@ -1,5 +1,9 @@
-DROP TABLE IF EXISTS circuit;
+DROP TABLE IF EXISTS course_pilote;
 DROP TABLE IF EXISTS course;
+DROP TABLE IF EXISTS circuit;
+DROP TABLE IF EXISTS pilote;
+DROP TABLE if EXISTS equipe;
+DROP VIEW IF EXISTS score_pilote;
 
 
 # creation table course
@@ -62,6 +66,54 @@ JOIN course_pilote as cp ON p.pilote_id = cp.pilote_id
 JOIN course as c ON c.course_id = cp.course_id);
 
 INSERT INTO circuit (pays, nom, longueur) VALUES ('France','Jose',2.5),
-                                                 ('France','Josette',2.5),
-                                                    ('France','Herve',2.5);
+                                                 ('Espagne','Josette',4),
+                                                    ('Belgique','Herve',5);
+
+#creation des courses
 INSERT INTO course (nom, date_course, circuit_id) VALUE ('test',CURDATE(),1);
+INSERT INTO course (nom, date_course, circuit_id) VALUE ('course ecolo','2024-10-09',2);
+
+
+INSERT INTO equipe (pays, nom, directeur_technique) VALUE ('FRANCE','Studi','Jose');
+INSERT INTO pilote (nom, prenom, nationalite, date_naissance, equipe_id) VALUE ('Michel','Augustin',
+                                                                               'Francaise',
+                                                                               '2023-10-09',
+                                                                               1);
+#creation du champ compteur_course
+ALTER TABLE pilote ADD COLUMN compteur_course INTEGER NOT NULL DEFAULT 0;
+
+INSERT INTO pilote (nom, prenom, nationalite, date_naissance, equipe_id) VALUE ('Josette','jesaispas',
+                                                                               'Francaise',
+                                                                               '2023-10-09',
+                                                                               1);
+
+
+
+#automatisation du compteur suite à une nouvelle insertion dans course_pilote
+CREATE OR REPLACE TRIGGER update_compteur_pilote
+    AFTER INSERT ON course_pilote
+    FOR EACH ROW
+    BEGIN
+       # on a le traitement d'incrémentation
+        UPDATE pilote
+        SET compteur_course = compteur_course + 1
+        WHERE pilote_id = NEW.pilote_id;
+    END;
+
+INSERT INTO course_pilote (pilote_id, course_id, position_pilote) VALUE (1,1,5);
+INSERT INTO course_pilote (pilote_id, course_id, position_pilote) VALUE (1,2,3);
+
+
+#appel de la vue : 1
+SELECT * FROM score_pilote;
+
+INSERT INTO course_pilote (pilote_id, course_id, position_pilote) VALUE (2,2,2);
+
+#appel de la vue : 2
+SELECT * FROM score_pilote;
+
+#affichage de toutes les données pilote
+SELECT * FROM pilote;
+
+
+
